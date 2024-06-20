c3964837a9c2b936bb2e79e8b34233def5abdcd7
Kelso94 <meher.sarkissian@outlook.fr>
2024-06-20 12:12:43 +0000

    alpahbeta

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ * Return: Allways 0 (Succes)
+ */
+
+/* betty style doc for function main goes there */
+
+int main(void)
+{
+	int numb = 0;
+
+	while (numb < 10)
+	{
+	printf("%d", numb);
+	numb++;
+	}
+	printf("\n");
+
+	return (0);
+}
